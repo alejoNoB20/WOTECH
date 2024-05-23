03d04db2d83b2767576e6ccc8c4293c2e1100f8a
unknown <taiels94@gmail.com>
2024-05-23 20:20:18 -0300

    Estructura del proyecto

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+import createError from 'http-errors';
+import express, { json, urlencoded } from 'express';
+import { join } from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import path from 'path'
+import dotenv from 'dotenv'
+
+//Internal
+import { __dirname } from './utils.js';
+// import indexRouter from './routes/index.js';
+// import usersRouter from './routes/users.js';
+
+const app = express();
+dotenv.config();
+
+// view engine setup
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/public')))
+
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running at port ${process.env.PORT}`);
+})
+export default app;
