412745b03738689551acda570cba682d1edaef93
Hiro 010 <taiels94@gmail.com>
2024-09-01 17:02:15 -0300

    upload clientsTest and readme.md (incomplete) for backend

--- backend/src/Components/Clients/clientsRouter.js
+++ backend/src/Components/Clients/clientsRouter.js
@@ -7,7 +7,7 @@ let clientsRouter = Router();
 clientsRouter.get('/', ClientsController.verTodos);
 clientsRouter.post('/create', clientsValidator.createAndUpdateClient, ClientsController.crear);
 clientsRouter.post('/delete/:dni_client', ClientsController.borrar);
-clientsRouter.get('/update/:dni_client', ClientsController.paginaActualizar);
+clientsRouter.patch('/update/:dni_client', ClientsController.paginaActualizar);
 clientsRouter.post('/update/:dni_client', clientsValidator.createAndUpdateClient, ClientsController.actualizar);
 clientsRouter.get('/search', clientsValidator.searchClient,ClientsController.filtrar);
 
