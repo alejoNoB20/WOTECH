e99d9a29a4eaf8e955c7d5660f07851dc6950056
alejo <alejoviviani12@gmail.com>
2025-02-17 22:03:35 -0300

    Wotech V1.3.3 (DB connection fixed)

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -2,24 +2,36 @@ import { Sequelize } from "sequelize";
 import fs from "fs";
 import 'dotenv/config';
 
-export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    // port: process.env.PORT,
-    dialect: 'mysql',
-    logging: false,
-    // dialectOptions: {
-    //     ssl: {
-    //         rejectUnauthorized: true,
-    //         require: true,
-    //         ca: fs.readFileSync("./src/database/ca.pem").toString()
-    //     }
-    // }
-});
+export let sequelize;
+
+if(process.env.PORT === '8080'){
+    // CONEXION A LA DB EN LOCAL
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        logging: false,
+    });
+} else {
+    // CONEXION A LA DB EN OVERVIEW
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        port: process.env.PORT,
+        dialect: 'mysql',
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: true,
+                require: true,
+                ca: fs.readFileSync("./src/database/ca.pem").toString()
+            }
+        }
+    });
+}
 
 export const updateDB = async () => {
     try{
         await sequelize.sync({ alter: true });
-        console.log('DB creada correctamente!');
+        (process.env.PORT === '8080') ? console.log('DB local creada correctamente!') : console.log('DB remota creada correctamente!')        
     }catch(err) {
         console.log('Error a la hora de actualiza la DB: ', err);
     }
@@ -28,7 +40,7 @@ export const updateDB = async () => {
 export const connectDB = async () => {
     try{
         await sequelize.sync({ alter: true });
-        console.log('Conectado correctamente a la DB!');
+        (process.env.PORT === '8080') ? console.log('Conectado correctamente a la DB local!') : console.log('Conectado correctamente a la DB remota!')
     }catch(err) {
         console.log('Error a la hora de actualiza la DB: ', err);
     }
@@ -38,7 +50,7 @@ export const clearDB = async () => {
     try{
         await sequelize.drop();
         await sequelize.sync({ force: true });
-        console.log('DB Reiniciada correctamente!');
+        (process.env.PORT === '8080') ? console.log('DB local reiniciada correctamente!') : console.log('DB remota reiniciada correctamente!')
     }catch(err) {
         console.log('Error a la hora de reiniciar la DB: ', err);
     }
